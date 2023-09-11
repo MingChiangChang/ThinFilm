8d658b06138833ae4bedf663d503675cead63dae
Hongrui Zhang <hz622@cornell.edu>
2023-09-10 21:02:58 -0400

    add auto data selection (based on uncertainty threshold) and auto thickness extraction

--- DataProcessing.py
+++ DataProcessing.py
@@ -4,33 +4,97 @@ from scipy.signal import medfilt
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 import numpy as np
+import os
 
 
-def process_data(path, left, right):
+def process_data(path, uncertainty_threshold=0.02):
     """
-    Read and crop the data within [left, right].
+    Read and crop the data within [left, right], and return cropped data, left, right.
 
     **Parameters:**
     path : str
         path of the data to be processed.
-    left : float
-        The lower bound of the wavelength.
-    right : float
-        The upper bound of the wavelength.
-
+    uncertainty_threshold : float
+        uncertainty threshold below which the data is selected.
     **Returns:**
-    data: panda DataFrame
+    data : panda DataFrame
         It returns the cropped data.
+    left : float
+        The lower bound of the cropped data.
+    right : float
+        The upper bound of the cropped data.
     """
     # Load the data
     data = pd.read_csv(path, names=[
         '# lambda', 'reflectance', 'uncertainty', 'raw', 'dark', 'reference', 'fit'], skiprows=13)
     data = data.rename(columns={'# lambda': 'wavelength'})
 
-    # Filter data based on wavelength
+    # Filter data based on uncertainty
+    uncertainty_filtered_data = data[data['uncertainty']
+                                     <= uncertainty_threshold]
+
+    # Determine the smallest and largest wavelengths
+    left = uncertainty_filtered_data['wavelength'].min()
+    right = uncertainty_filtered_data['wavelength'].max()
+
+    print("left = ", left)
+    print("right = ", right)
+
+    # Filter data based on determined wavelength boundaries
     data = data[(data['wavelength'] >= left) & (data['wavelength'] <= right)]
 
-    return data
+    return data, left, right
+
+
+def extract_layer_thicknesses(file_path):
+    """
+    Extract the layer thicknesses of materials from the CSV file.
+
+    The function reads a file and extracts information between
+    "# SAMPLE STACK" and the subsequent "#   substrate ...". 
+    For all rows within this section (except for the substrate),
+    the function captures the thickness value of each material.
+
+    **Parameters:**
+    - file_path (str): The path to the CSV file.
+
+    **Returns:**
+    - list of float: A list containing the thickness values of each material.
+    """
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    sample_data_lines = []
+    capture = False  # Flag to determine whether we should capture lines
+
+    for line in lines:
+        stripped_line = line.strip().replace(",", "")
+
+        if "# SAMPLE STACK" in stripped_line:
+            capture = True  # Start capturing after encountering "# SAMPLE STACK"
+            continue
+        elif capture and "#   substrate" in stripped_line:
+            break
+        elif "# END" in stripped_line:  # Handle cases where "# END" comes before the "# SAMPLE STACK"
+            capture = False  # Reset the flag
+            continue
+        elif capture:
+            sample_data_lines.append(stripped_line)
+
+    # Print the lines between "# SAMPLE STACK" and "#   substrate ..."
+    print("\nCaptured lines:")
+    for line in sample_data_lines:
+        print(line.strip())
+
+    layer_thicknesses = []
+
+    for line in sample_data_lines:
+        grids = line.split()
+        thickness = float(grids[3])
+        layer_thicknesses.append(thickness)
+
+    return layer_thicknesses
 
 
 def data_smoothing(data, kernel_size=11, sigma=20, gaussian_filter=True):
@@ -75,19 +139,23 @@ def data_smoothing(data, kernel_size=11, sigma=20, gaussian_filter=True):
     return smoothed_data
 
 
-def plot_reflectance(data, multilayer, layer_index=1, smooth=False, calculated_data=None, optimal_data=None):
+def plot_reflectance(data, multilayer, layer_index=1, smooth=False, calculated_data=None, optimal_data=None, save=False, filepath='', filename='reflectance_plot.png'):
     """
     Plot selected datasets: original, smoothed, and/or optimal reflectance of a specific layer in a multilayer.
 
     Parameters:
-    - data (pd.DataFrame): Experimental data where `data['wavelength']` is the wavelength, 
-                           and `data['reflectance']` is the experimental reflectance.
-    - smooth (bool, optional): Whether to smooth the data and plot the smoothed data or not. Defaults to False.
-    - calculated_data (np.array, optional): the calculated reflectance data (before optimization). Defaults to None.
+    - data (pd.DataFrame): Experimental data with columns 'wavelength' and 'reflectance' representing the wavelength and experimental reflectance respectively.
+    - multilayer (object): A multilayer object containing layer information.
+    - layer_index (int, optional): Index of the specific layer in the multilayer to be plotted. Defaults to 1.
+    - smooth (bool, optional): Whether to smooth the data and plot the smoothed data. Defaults to False.
+    - calculated_data (np.array, optional): The calculated reflectance data (before optimization). Defaults to None.
     - optimal_data (np.array, optional): The optimal reflectance data. Defaults to None.
+    - save (bool, optional): Whether to save the generated plot to a file. Defaults to False.
+    - filepath (str, optional): Directory path where the plot will be saved. Defaults to the current working directory.
+    - filename (str, optional): Name of the file to save the plot as. Defaults to 'reflectance_plot.png'.
 
     Returns:
-    None. Displays the plot.
+    - None. Displays or saves the plot based on provided parameters.
     """
     plt.figure(figsize=(8, 6))
 
@@ -127,12 +195,31 @@ def plot_reflectance(data, multilayer, layer_index=1, smooth=False, calculated_d
     plt.ylabel('Reflectance')
     plt.legend()
     plt.tight_layout()
+
+    # Choose to save the plot or not
+    plt.tight_layout()
+    if save:
+        complete_path = os.path.join(filepath, filename)
+        plt.savefig(complete_path, dpi=300)
     plt.show()
 
 
-def plot_nk(data, optimized_params, n_points, multilayer, layer_index=1):
+def plot_nk(data, optimized_params, n_points, multilayer, layer_index=1, save=False, filepath='', filename='nk_plot.png'):
     """
-    Plot n, k before and after optimization.
+    Plot refractive index (n) and extinction coefficient (k) before and after optimization for a specific material layer.
+
+    Parameters:
+    - data (pd.DataFrame): Experimental data with a 'wavelength' column representing the wavelength range.
+    - optimized_params (np.array): Array containing optimized values of refractive index and extinction coefficient.
+    - n_points (int): Number of data points in the 'n' or 'k' dataset.
+    - multilayer (object): A multilayer object containing layer information.
+    - layer_index (int, optional): Index of the specific layer in the multilayer whose properties are to be plotted. Defaults to 1.
+    - save (bool, optional): Whether to save the generated plot to a file. Defaults to False.
+    - filepath (str, optional): Directory path where the plot will be saved. Defaults to the current working directory.
+    - filename (str, optional): Name of the file to save the plot as. Defaults to 'nk_plot.png'.
+
+    Returns:
+    - None. Displays or saves the plot based on provided parameters.
     """
     # Get the wavelength range from the data
     wavelength_range = data['wavelength']
@@ -201,4 +288,9 @@ def plot_nk(data, optimized_params, n_points, multilayer, layer_index=1):
     ax2.set_title(f'Extinction coefficient (k) vs wavelength for {material}')
 
     plt.tight_layout()
+
+    # Choose to save the plot or not
+    if save:
+        complete_path = os.path.join(filepath, filename)
+        plt.savefig(complete_path, dpi=300)
     plt.show()
